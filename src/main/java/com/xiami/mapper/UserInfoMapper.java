06bbdd1ba43ac836a58d2b4d33f3caeef99776a5
hongchunliu <627751877@qq.com>
2017-05-17 17:38:04 +0800

    xiamia框架搭建

---
+++ src/main/java/com/xiami/mapper/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.xiami.mapper;
+
+import com.xiami.po.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
---
+++ src/main/java/com/xiami/po/UserInfo.java
@@ -0,0 +1,175 @@
+package com.xiami.po;
+
+import java.util.Date;
+
+public class UserInfo {
+    private Integer id;
+
+    private String ipAddress;
+
+    private String editionNumber;
+
+    private String token;
+
+    private Integer userId;
+
+    private Date loseTime;
+
+    private Date insertTime;
+
+    private Date updateTime;
+
+    private Integer deleted;
+
+    private String param1;
+
+    private String param2;
+
+    private String param3;
+
+    private String param4;
+
+    private String param5;
+
+    private String unionid;
+
+    private String alipayUserId;
+
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress == null ? null : ipAddress.trim();
+    }
+
+    public String getEditionNumber() {
+        return editionNumber;
+    }
+
+    public void setEditionNumber(String editionNumber) {
+        this.editionNumber = editionNumber == null ? null : editionNumber.trim();
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token == null ? null : token.trim();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getLoseTime() {
+        return loseTime;
+    }
+
+    public void setLoseTime(Date loseTime) {
+        this.loseTime = loseTime;
+    }
+
+    public Date getInsertTime() {
+        return insertTime;
+    }
+
+    public void setInsertTime(Date insertTime) {
+        this.insertTime = insertTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String param1) {
+        this.param1 = param1 == null ? null : param1.trim();
+    }
+
+    public String getParam2() {
+        return param2;
+    }
+
+    public void setParam2(String param2) {
+        this.param2 = param2 == null ? null : param2.trim();
+    }
+
+    public String getParam3() {
+        return param3;
+    }
+
+    public void setParam3(String param3) {
+        this.param3 = param3 == null ? null : param3.trim();
+    }
+
+    public String getParam4() {
+        return param4;
+    }
+
+    public void setParam4(String param4) {
+        this.param4 = param4 == null ? null : param4.trim();
+    }
+
+    public String getParam5() {
+        return param5;
+    }
+
+    public void setParam5(String param5) {
+        this.param5 = param5 == null ? null : param5.trim();
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid == null ? null : unionid.trim();
+    }
+
+    public String getAlipayUserId() {
+        return alipayUserId;
+    }
+
+    public void setAlipayUserId(String alipayUserId) {
+        this.alipayUserId = alipayUserId == null ? null : alipayUserId.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+}
